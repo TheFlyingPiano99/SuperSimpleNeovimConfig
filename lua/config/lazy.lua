e4facd34db5db5f2d594cd9fc5750249c82ae108
TheFlyingPiano99 <simonzolika1999@gmail.com>
2026-03-10 13:24:29 +0100

    Removed .config / .configs from the treesitter config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -36,7 +36,7 @@ require("lazy").setup({
             lazy = false,
             build = ":TSUpdate",
             config = function()
-                require("nvim-treesitter.config").setup({
+                require("nvim-treesitter").setup({
                     auto_install = false,
                     ensure_installed = {
                         "c", "cpp", "cmake", "glsl", "python", "rust",
